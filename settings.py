f08a0bcb6ae9a53403672eea0a2ff3ec4933ec3c
Shumpy09 <szymon.hadada@gmail.com>
2021-10-05 03:56:25 +0200

    modify settings.py

--- settings.py
+++ settings.py
@@ -10,18 +10,33 @@ class Settings():
         self.bg_color = (230, 230, 230)
         
         # Ustawienia dotyczące statku
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Ustawienia dotyczące pocisku
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3            # ograniczenie ilości pocisków
 
         # Ustawienia dotyczące obcego
-        self.alien_speed = 0.5
         self.fleet_drop_speed = 10          # prędkość poruszania się floty w dół
-        # Wartość fleet_direction wynosząca 1 oznacza prawo, natomiast -1 oznacza lewo
+
+        # Łatwa zmiana szybkości gry
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Inicjalizacja ustawień, które ulegają zmianie w trakcie gry"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # Wartość fleet_direction wynosząca 1 oznacza prawo, natomiast -1 lewo
         self.fleet_direction = 1
+    
+    def increase_speed(self):
+        """Zmiana ustawień dotyczących szybkości"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
